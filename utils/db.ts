39e3d1f08697562f511ba4d94f6e3556423cf88a
Mutse Young <yyhoo2.young@gmail.com>
2024-07-25 12:53:52 +0800

    Update db.ts
    Add llama-3.1-8b-instruct

--- utils/db.ts
+++ utils/db.ts
@@ -65,6 +65,11 @@ export const textGenModels: Model[] = [{
     endpoint: 'chat/completions',
     type: 'chat'
 }, {
+    id: '@hf/meta-llama/llama-3.1-8b-instruct',
+    name: 'Llama3.1-8b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
+}, {
     id: '@hf/meta-llama/llama-3-8b-instruct',
     name: 'Llama3-8b-instruct',
     provider: 'workers-ai',
