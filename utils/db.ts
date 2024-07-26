45b5a229ed04e2fe262966067f0c96f6de93c9a8
Mutse Young <yyhoo2.young@gmail.com>
2024-07-26 08:42:46 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -65,12 +65,12 @@ export const textGenModels: Model[] = [{
     endpoint: 'chat/completions',
     type: 'chat'
 }, {
-    id: '@hf/meta/llama-3.1-8b-instruct',
+    id: '@cf/meta/llama-3.1-8b-instruc',
     name: 'Llama3.1-8b-instruct',
     provider: 'workers-ai',
     type: 'chat'
 }, {
-    id: '@hf/meta-llama/llama-3-8b-instruct',
+    id: '@cf/meta-llama/llama-3-8b-instruct',
     name: 'Llama3-8b-instruct',
     provider: 'workers-ai',
     type: 'chat'
